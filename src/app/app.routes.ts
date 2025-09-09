8cac5192d00d066497f9e18dedacc35ff864b5f6
y.ustinov2004 <y.ustinov2004@gmail.com>
2025-09-09 09:55:45 +0000

    refactor: adding routes for auth section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Recipes } from './recipes/recipes';
+import { Auth } from './auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Recipes,
+        title: 'Recipes',
+    },
+    {
+        path: 'auth',
+        component: Auth,
+        title: 'Authentication',
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { Recipes } from './recipes/recipes';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Recipes, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
+
 })
 export class App {
   protected readonly title = signal('tasti_front');
