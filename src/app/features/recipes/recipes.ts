6eb27f892058aac8781943c58b034a53b978d1f8
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2025-09-12 13:42:55 +0200

    feat: button to add recipes TO DO: create-recipe page

--- src/app/features/recipes/recipes.ts
+++ src/app/features/recipes/recipes.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RecipesList } from './recipes-list/recipes-list';
 import RecipesService from './recipes.service';
 import { MainLayout } from '../../shared/layout/main-layout/main-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-recipes',
-  imports: [RecipesList, MainLayout],
+  imports: [RecipesList, MainLayout, MatButtonModule, MatIconModule],
   templateUrl: './recipes.html',
   styleUrl: './recipes.css',
   providers: [RecipesService],
