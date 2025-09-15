9f75351a3b1d7b32d065b6639e628db979e77f87
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2025-09-15 22:06:37 +0200

    feat: add auth button inside sidebar

--- src/app/shared/layout/main-layout/sidebar/sidebar.ts
+++ src/app/shared/layout/main-layout/sidebar/sidebar.ts
@@ -1,10 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatMenuModule } from '@angular/material/menu';
 import { ThemeService } from '../../../../core/services/theme.service';
 import { LogoComponent } from '../../../components/logo/logo';
+import { Router } from '@angular/router';
+import { UserService } from '../../../../core/services/user.service';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -13,5 +16,26 @@ import { LogoComponent } from '../../../components/logo/logo';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  router = inject(Router);
+
   themeService = inject(ThemeService);
+  userService = inject(UserService);
+  authService = inject(AuthService);
+
+  onAuthButtonClick() {
+    if (this.userService.isLoggedIn()) {
+      // TODO: Implement account page navigation
+      // this.router.navigate(['/account']);
+    } else {
+      this.router.navigate(['/auth/login']);
+    }
+  }
+
+  getAuthButtonIcon(): string {
+    return this.userService.isLoggedIn() ? 'account_circle' : 'login';
+  }
+
+  getAuthButtonTooltip(): string {
+    return this.userService.isLoggedIn() ? 'Account' : 'Login';
+  }
 }
