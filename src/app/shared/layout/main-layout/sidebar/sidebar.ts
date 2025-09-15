31cc0dd0b8b44d1de2c2c4a0a285cd33f7a80655
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2025-09-16 00:15:29 +0200

    feat: move github and theme switcher to separate sub-menu

--- src/app/shared/layout/main-layout/sidebar/sidebar.ts
+++ src/app/shared/layout/main-layout/sidebar/sidebar.ts
@@ -1,19 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatMenuModule } from '@angular/material/menu';
-import { ThemeService } from '../../../../core/services/theme.service';
-import { LogoComponent } from '../../../components/logo/logo';
 import { Router } from '@angular/router';
+import { ThemeService } from '../../../../core/services/theme.service';
 import { UserService } from '../../../../core/services/user.service';
 import { AuthService } from '../../../../core/services/auth.service';
+import { LogoComponent } from '../../../components/logo/logo';
 
 @Component({
   selector: 'app-sidebar',
   imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatMenuModule, LogoComponent],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Sidebar {
   router = inject(Router);
