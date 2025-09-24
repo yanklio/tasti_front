523abc4157954f6a4c3d7011709d4fdeb406c66b
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2025-09-24 17:14:39 +0200

    feat: add paginator block to recipes-list

--- src/app/features/recipes/recipes-list/recipes-list.ts
+++ src/app/features/recipes/recipes-list/recipes-list.ts
@@ -6,12 +6,13 @@ import { RECIPES_ROUTES } from '../constants';
 import { LoaderComponent } from '../../../shared/loader/loader';
 import RecipeListService from '../services/recipe-list.service';
 import { RecipesCard } from './recipes-card/recipes-card';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.html',
   styleUrl: './recipes-list.css',
-  imports: [RecipesCard, MatButtonModule, MatIconModule, LoaderComponent],
+  imports: [RecipesCard, MatButtonModule, MatIconModule, LoaderComponent, MatPaginatorModule],
 })
 export class RecipesList {
   router = inject(Router);
