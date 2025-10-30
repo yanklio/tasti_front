5d561d4ce6148185725c8b03326cb41b0cb2f23c
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2025-10-30 15:32:55 +0100

    feat: debounce input of search bar

--- src/app/features/recipes/search-bar/search-bar.ts
+++ src/app/features/recipes/search-bar/search-bar.ts
@@ -1,31 +1,48 @@
-import { Component, signal, output, ChangeDetectionStrategy } from '@angular/core';
+import { Component, signal, inject, ChangeDetectionStrategy, effect } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import RecipeListService from '../services/recipe-list.service';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './search-bar.html',
   styleUrls: ['./search-bar.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchBar {
-  searchTerm = signal('');
-  search = output<string>();
+  private readonly recipeListService = inject(RecipeListService);
 
-  onInput(event: Event) {
-    const value = (event.target as HTMLInputElement).value;
-    this.searchTerm.set(value);
-    this.search.emit(value);
+  readonly searchTermSignal = signal<string>('');
+  readonly searchTerm = new FormControl<string>('');
+
+  constructor() {
+    effect(() => {
+      this.searchTerm.valueChanges
+        .pipe(debounceTime(500), distinctUntilChanged())
+        .subscribe((res) => {
+          this.searchTermSignal.set(res || '');
+          console.log('Performing search for:', res);
+        });
+    });
   }
 
   onIconClick() {
-    if (this.searchTerm()) {
-      this.searchTerm.set('');
-      this.search.emit('');
+    if (this.searchTermSignal()) {
+      this.searchTerm.setValue('');
     }
   }
 }
